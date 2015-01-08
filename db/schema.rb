12c28f6ef6aee23b79cacb0f410d77607841d472
lokada <lyokada@ucsc.edu>
2015-01-08 10:33:08 -0800

    static_pages/home added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150107235449) do
+ActiveRecord::Schema.define(version: 20150108000513) do
 
   create_table "messages", force: true do |t|
     t.text     "content"
