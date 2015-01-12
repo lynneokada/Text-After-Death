5cfa1e6e904a102f96d24d3204ecfeb659bba342
Harlan Kellaway <hkellaway.tech@gmail.com>
2015-01-12 10:36:44 -0800

    Updates User create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
+    @user = User.find(params[:id])
   end
 
   # GET /users/new
@@ -61,6 +62,10 @@ class UsersController < ApplicationController
       format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
       format.json { head :no_content }
     end
+  end
+
+  def get_user_type
+    @user.user_type
   end
 
   private
