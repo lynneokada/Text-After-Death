8bc4f450a3264ff5717fe196592a3ac12330936d
lokada <lyokada@ucsc.edu>
2015-01-12 13:34:42 -0800

    links to all buttons, home layout updated

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,8 @@
 class Message < ActiveRecord::Base
   #relationships
+  belongs_to :sender 
+  has_many :receivers
 
   #validations
-  validates :content, presence: true
+  validates :content, :presence => true
 end
