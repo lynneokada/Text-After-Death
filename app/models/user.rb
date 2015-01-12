abfdb6b32f7e2717e85b030d72893e5ae7bc6a24
lokada <lyokada@ucsc.edu>
2015-01-12 15:19:04 -0800

    new user error_messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,19 @@
 class User < ActiveRecord::Base
 
   # validations
+  before_save { self.email = email.downcase }
   validates :name,  :presence => true, :length => { maximum: 50 }
-  # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  # validates :email, presence:true,
-  #                   length: {maximum: 255},
-  #                   format: {with: VALID_EMAIL_REGEX},
-  #                   uniqueness: {case_sensitive: false}
-  validates :password, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence:true,
+                    length: {maximum: 255},
+                    format: {with: VALID_EMAIL_REGEX},
+                    uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, length: { minimum: 6 }
 
   self.inheritance_column = :user_type
+
+
 
   # We will need a way to know which types with subclass the User model
   def self.user_types
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150109221806) do
+ActiveRecord::Schema.define(version: 20150112224723) do
 
   create_table "messages", force: true do |t|
     t.text     "content"
@@ -32,6 +32,9 @@ ActiveRecord::Schema.define(version: 20150109221806) do
     t.string   "user_type"
     t.string   "phone_number"
     t.string   "public_key"
+    t.string   "password_digest"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
