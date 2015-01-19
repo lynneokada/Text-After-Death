36cbccdf09ce8052cef178e8c4224843af01a7d1
lokada <lyokada@ucsc.edu>
2015-01-19 10:48:27 -0800

    through deleted from message belongs_to relationship

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,7 @@
 class Message < ActiveRecord::Base
   #relationships
   has_many :receivers, through: :relationships, dependent: :destroy
-  belongs_to :user, through: :relationships, dependent: :destroy
+  belongs_to :user, dependent: :destroy
 
   #validations
   validates :content, :presence => true
