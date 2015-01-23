30cb47abbb99a7afb012653edd27ec3c6af0c3a2
Harlan Kellaway <hkellaway.tech@gmail.com>
2015-01-23 12:58:07 -0800

    Updatesr Relationship model to remove validation of user_id and message_id

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -3,7 +3,5 @@ class Relationship < ActiveRecord::Base
   belongs_to :reveiver, class_name: "Receiver"
   belongs_to :message,  class_name: "Message"
 
-  validates :user_id, presence: true
   validates :receiver_id, presence: true
-  validates :message_id, presence: true
 end
