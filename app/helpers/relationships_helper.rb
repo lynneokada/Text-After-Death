87ea11deafaf0ed55efbe640438ecec5707e3c14
Harlan Kellaway <hkellaway.tech@gmail.com>
2015-01-29 11:18:51 -0800

    Starts implementing relationshp creation

--- app/helpers/relationships_helper.rb
+++ app/helpers/relationships_helper.rb
@@ -1,2 +1,14 @@
 module RelationshipsHelper
+
+def create_relationships(message_receiver_ids, message_id)
+
+  message_receiver_ids.each do |receiver_id|
+    @relationship = Relationship.create
+
+    @relationship.user_id = current_user.id
+    @relationship.receiver_id = receiver_id
+    @relationship.message_id = message_id
+
+  end
+
 end
