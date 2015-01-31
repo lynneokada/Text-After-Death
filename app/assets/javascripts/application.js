4ecb7ed614edfb53fe3c0ff4ca12cc1cd78e7e9f
lokada <lyokada@ucsc.edu>
2015-01-30 21:18:54 -0800

    home page updated

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,10 @@
 //= require bootstrap
 //= require webcomponentsjs/webcomponents
 //= require_tree .
+
+function blinker() {
+  $('.blink_me').fadeOut(500);
+  $('.blink_me').fadeIn(500);
+}
+
+setInterval(blinker, 1250);
