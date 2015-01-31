f4f96e96ee9faf514d6fbe80d7a49bdff54dcd0f
lokada <lyokada@ucsc.edu>
2015-01-30 16:59:18 -0800

    image

--- app/models/message.rb
+++ app/models/message.rb
@@ -7,6 +7,7 @@ class Message < ActiveRecord::Base
   # validations
   validates :content, :presence => true
   validates :date, :presence => true
+  validates :receivers, :presence => true
   accepts_nested_attributes_for :relationships
 
 end
